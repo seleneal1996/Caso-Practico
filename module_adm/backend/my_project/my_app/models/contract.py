298bdf1804ad3ed28e7e1220035085c0419993fd
Pejelagarta <selenemsb@hotmail.com>
2024-03-12 15:46:38 -0500

    Diagrama ER

--- module_adm/backend/my_project/my_app/models/contract.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-from .staff import Collaborator
-
-class Contract(models.Model):
-    collaborator = models.ForeignKey(Collaborator, on_delete=models.CASCADE)
-    start_date = models.DateField()
-    end_date = models.DateField()
-    eps = models.CharField(max_length=50)
-    life_insurance = models.CharField(max_length=50)
